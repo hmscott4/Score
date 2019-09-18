4d6435c7fbe6ab0e209dacfb838381fdf730e956
hmscott4 <hugh.scott@microsoft.com>
2019-09-18 14:41:40 -0400

    Add database scripts for AlertResolutionState; Add tool tips to Performance Charts. Rename deployment script.

---
+++ Score.Database/_Deployment/Script.Release.1.0.3.0.sql
@@ -0,0 +1,36 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+/**********************************************************************
+* ADD IsOpen Column to scom.AlertResolutionState
+* HMS, Issue 19,20,21
+* 2019/09/18
+***********************************************************************/
+-- Add Columns
+ALTER TABLE scom.AlertResolutionState
+ADD [IsOpen] bit DEFAULT 1
+GO
+
+-- Update Column, default is 1
+UPDATE scom.AlertResolutionState
+SET [IsOpen] = 1
+WHERE Name != N'Closed'
+GO
+
+-- Update IsOpen, set 'Closed' to 0
+UPDATE scom.AlertResolutionState
+SET [IsOpen] = 0
+WHERE Name = N'Closed'
+GO
+
+-- Set Column to NOT NULL
+ALTER TABLE scom.AlertResolutionState ALTER COLUMN [IsOpen] BIT NOT NULL
+GO
---
+++ Score.Database/scom/Stored Procedures/spAlertResolutionStateGet.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROC [scom].[spAlertResolutionStateGet]
+	@ResolutionState INT = NULL,
+	@IsOpen BIT = 1
+
+AS
+
+SET NOCOUNT ON
+SET XACT_ABORT OFF
+
+SELECT 
+	ResolutionState, [Name]
+FROM 
+	scom.AlertResolutionState
+WHERE
+	Active = 1
+	AND (@ResolutionState IS NULL OR ResolutionState = @ResolutionState)
+	AND (@IsOpen IS NULL OR IsOpen = @IsOpen)
+ORDER BY
+	ResolutionState
